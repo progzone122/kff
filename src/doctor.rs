36c430596fdbc9766ad23bb6568022d43fa961a3
diablo <progzone122@gmail.com>
2025-07-16 14:01:07 +0300

    rollback path

--- src/doctor.rs
+++ src/doctor.rs
@@ -20,7 +20,7 @@ pub fn run() -> Result<()> {
 }
 
 pub fn read_first_lines(ksdk: &str) -> Result<Vec<String>> {
-    let path = Path::new(ksdk).join("meson-crosscompile.txt");
+    let path = Path::new(ksdk);
 
     let file = File::open(path).map_err(|err| anyhow!("File '{}' not found: {}", ksdk, err))?;
     let reader = BufReader::new(file);
